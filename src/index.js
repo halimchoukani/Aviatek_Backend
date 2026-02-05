39ec383bef78591fe598d44e2a880020fd5323e1
MbarkAmal <150669674+MbarkAmal@users.noreply.github.com>
2026-02-05 21:34:39 +0100

    create pilot router with GET endpoints

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.js";
+import pilotsRoutes from "./routes/pilots.js";
+
 
 dotenv.config();
 const app = express();
@@ -8,6 +10,10 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
+app.use("/api/pilots", pilotsRoutes);
+
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
